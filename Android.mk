d9cb48c13366eac8b9cee65488253cbd5dca578b
sudosurootdev <sudosurootdev@gmail.com>
2014-07-08 08:13:32 -0400

    In case -ffast-math CFLAG is set. Cannot be set with sqlite.
    Change-Id: I6b50bcf58ce4264997b37d525bae8249516c4ed2

--- Android.mk
+++ Android.mk
@@ -22,7 +22,7 @@ LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_STATIC_LIBRARIES := libc libcutils
 LOCAL_C_INCLUDES := external/sqlite/dist
 LOCAL_SRC_FILES := Superuser/jni/su/su.c Superuser/jni/su/daemon.c Superuser/jni/su/activity.c Superuser/jni/su/db.c Superuser/jni/su/utils.c Superuser/jni/su/pts.c ../../sqlite/dist/sqlite3.c
-LOCAL_CFLAGS := -DSQLITE_OMIT_LOAD_EXTENSION -DREQUESTOR=\"$(SUPERUSER_PACKAGE)\"
+LOCAL_CFLAGS := -DSQLITE_OMIT_LOAD_EXTENSION -DREQUESTOR=\"$(SUPERUSER_PACKAGE)\" -fno-fast-math
 
 ifdef SUPERUSER_PACKAGE_PREFIX
   LOCAL_CFLAGS += -DREQUESTOR_PREFIX=\"$(SUPERUSER_PACKAGE_PREFIX)\"
--- Superuser/jni/Android.mk
+++ Superuser/jni/Android.mk
@@ -6,6 +6,7 @@ LOCAL_MODULE := su
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_STATIC_LIBRARIES := sqlite3 libcutils libc
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/sqlite3
+LOCAL_CFLAGS := -fno-fast-math
 LOCAL_SRC_FILES := su/su.c su/activity.c su/db.c su/utils.c su/daemon.c su/pts.c
 include $(BUILD_EXECUTABLE)
 
@@ -16,6 +17,7 @@ LOCAL_MODULE := reboot
 LOCAL_LDFLAGS := -llog
 LOCAL_STATIC_LIBRARIES := sqlite3
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/sqlite3
+LOCAL_CFLAGS := -fno-fast-math
 LOCAL_SRC_FILES := reboot/reboot.c
 include $(BUILD_EXECUTABLE)
 
@@ -24,5 +26,5 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := sqlite3
 LOCAL_SRC_FILES := sqlite3/sqlite3.c
-LOCAL_CFLAGS := -DSQLITE_OMIT_LOAD_EXTENSION
+LOCAL_CFLAGS := -DSQLITE_OMIT_LOAD_EXTENSION -fno-fast-math
 include $(BUILD_STATIC_LIBRARY)
